a08ac724143c6001fb2b592a4493926a64edd103
Ricardo Joel Gonzalez Velasco <70970994+ricardgo403@users.noreply.github.com>
2020-10-18 14:05:47 -0500

    feat: oddGenerator function is now implemented.
    The main calls the oddGenerator in a loop.

--- generator/main.go
+++ generator/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func oddGenerator() func() uint {
+	i := uint(1)
+	return func() uint {
+		var odd = i
+		i += 2
+		return odd
+	}
+}
+
+func main() {
+	nextOdd := oddGenerator()
+	for i := 0; i < 10; i++ {
+		fmt.Println(nextOdd())
+	}
 }
