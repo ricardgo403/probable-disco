46293bb483a3ff35bdf695d96ecfc042a394e6eb
Ricardo Joel Gonzalez Velasco <70970994+ricardgo403@users.noreply.github.com>
2020-10-18 13:42:44 -0500

    feat: Fibonnaci recursive is now implemented.
    It is parameterized, so you need to pass the value. In order to print
    the whole serie till the point you wanted, I added a slice which will
    be fill during the loop.

--- fibonacci/main.go
+++ fibonacci/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func fibonacci(number int64) int64 {
+	if number == 0 || number == 1 {
+		return number
+	}
+
+	return (fibonacci(number-2) + fibonacci(number-1))
+}
+
+func main() {
+	a := 15
+	fibonacciSerie := []int64{}
+
+	for i := 0; i < a; i++ {
+		fibonacciSerie = append(fibonacciSerie, fibonacci(int64(i)))
+	}
+	fmt.Println(fibonacciSerie)
 }
