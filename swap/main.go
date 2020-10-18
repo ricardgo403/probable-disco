e8d3ae48cfad389eaa052ee8f9133213ce9f6776
Ricardo Joel Gonzalez Velasco <70970994+ricardgo403@users.noreply.github.com>
2020-10-18 14:13:40 -0500

    feat: Swap function is now implemented.
    It receives two pointers and swaps the contained values.

--- swap/main.go
+++ swap/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func swap(a, b *int) {
+	*a, *b = *b, *a
+}
+
+func main() {
+	a := 15
+	b := 999
+	fmt.Println(a, b)
+	swap(&a, &b)
+	fmt.Println(a, b)
 }
