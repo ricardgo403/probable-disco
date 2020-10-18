9335bc3db61fafb085bec96ff0a756bf418e04cd
Ricardo Joel Gonzalez Velasco <70970994+ricardgo403@users.noreply.github.com>
2020-10-18 13:57:16 -0500

    feat: Sum function is implemented.
    This function will return the biggest value passed to it.

--- variadic/main.go
+++ variadic/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func sum(args ...int) int {
+	masGrande := 0
+	for _, v := range args {
+		if v > masGrande {
+			masGrande = v
+		}
+	}
+	return masGrande
+}
+
+func main() {
+	a := []int{987, 564, 1000, 999, 1, 2, 0, -1, 1001, 9845789}
+	fmt.Println(sum(a...))
 }
